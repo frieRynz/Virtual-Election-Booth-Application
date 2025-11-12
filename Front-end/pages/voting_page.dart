d65c98db2330ca2291201b91d0e5766c66932f28
kOng <phurinchaz@gmail.com>
2025-11-12 11:30:50 +0700

    voting_page with picture

--- Front-end/pages/voting_page.dart
+++ Front-end/pages/voting_page.dart
@@ -1,113 +1,160 @@
 import 'package:flutter/material.dart';
-import '/services/auth_service.dart';
-import '/pages/home_screen.dart';
 
 class VotingPage extends StatefulWidget {
-  final int voterId;
-  const VotingPage({
-    super.key,
-    required this.voterId,
-  });
+  const VotingPage({super.key});
 
   @override
   _VotingPageState createState() => _VotingPageState();
+
 }
 
+
 class _VotingPageState extends State<VotingPage> {
-  final AuthService _authService = AuthService();
-  bool _isLoading = false;
-  String _errorMessage = '';
-
-  void _handleVote(int candidateId) async {
-    setState(() {
-      _isLoading = true;
-      _errorMessage = '';
-    });
-
-    try {
-      await _authService.submitVote(candidateId); 
-
-      if (mounted) {
-        showDialog(
-          context: context,
-          barrierDismissible: false,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: const Text('Vote Submitted'),
-              content: const Text('Thank you for voting!'),
-              actions: [
-                TextButton(
-                  child: const Text('OK'),
-                  onPressed: () {
-                    Navigator.of(context).pushAndRemoveUntil(
-                      MaterialPageRoute(builder: (_) => HomeScreen()),
-                      (route) => false,
-                    );
-                  },
-                ),
-              ],
-            );
-          },
-        );
-      }
-    } catch (e) {
-      setState(() {
-        _errorMessage = e.toString().replaceFirst('Exception: ', '');
-        _isLoading = false;
-      });
-    }
-  }
+  bool is1Checked = false; 
+  bool is2Checked = false; 
+  bool is3Checked = false; 
+  bool is4Checked = false; 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Cast Your Vote'),
+        title: const Text('Election Page'),
         centerTitle: true,
       ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: _isLoading
-              ? const CircularProgressIndicator() 
-              : Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        _buildCandidateButton('Candidate No.1', 1),
-                        const SizedBox(width: 20),
-                        _buildCandidateButton('Candidate No.2', 2),
-                      ],
-                    ),
-                    const SizedBox(height: 40),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        _buildCandidateButton('Candidate No.3', 3),
-                        const SizedBox(width: 20),
-                        _buildCandidateButton('Candidate No.4', 4),
-                      ],
-                    ),
-                    if (_errorMessage.isNotEmpty)
-                      Padding(
-                        padding: const EdgeInsets.only(top: 20.0),
-                        child: Text(
-                          _errorMessage,
-                          style: const TextStyle(color: Colors.red, fontSize: 16),
-                        ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  
+                  Column(
+                    children: [
+                      Container(width: 180,height: 180,color: Colors.green,
+                      child: Column(
+                        children: [
+                          Expanded(child: 
+                          Image.network('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+                            fit: BoxFit.contain,
+                          ),),
+                          Text(
+                            'Mr. Bulbasaur',
+                            style: TextStyle(fontSize: 20, color: Colors.black),
+                          ),
+                          const SizedBox(height: 10),
+                        ]
                       ),
-                  ],
-                ),
+                    ),
+                    const SizedBox(height: 10),
+                    _selectCandidateButton('Candidate No.1', is1Checked,
+                                            (val) {setState(() => is1Checked = val!);}),
+                    ],
+                  ),
+                  
+                  const SizedBox(width: 20),
+
+                  Column(
+                    children: [
+                      Container(width: 180,height: 180,color: Colors.red[200],
+                      child: Column(
+                        children: [
+                          Expanded(child: 
+                          Image.network('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
+                            fit: BoxFit.contain,
+                          ),),
+                          Text(
+                            'Mr.Charmander',
+                            style: TextStyle(fontSize: 20, color: Colors.black),
+                          ),
+                          const SizedBox(height: 10),
+                        ]
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    _selectCandidateButton('Candidate No.2', is2Checked,
+                                            (val) {setState(() => is2Checked = val!);}),
+                    ],
+                  ),
+
+                ],
+              ),
+
+              const SizedBox(height: 20),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  Column(
+                    children: [
+                      Container(width: 180,height: 180,color: Colors.blue[50],
+                      child: Column(
+                        children: [
+                          Expanded(child: 
+                          Image.network('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png',
+                            fit: BoxFit.contain,
+                          ),),
+                          Text(
+                            'Mr.Squirtle',
+                            style: TextStyle(fontSize: 20, color: Colors.black),
+                          ),
+                          const SizedBox(height: 10),
+                        ]
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    _selectCandidateButton('Candidate No.3', is3Checked,
+                                            (val) {setState(() => is3Checked = val!);}),
+                    ],
+                  ),
+
+                  const SizedBox(width: 20),
+
+                  Column(
+                    children: [
+                      Container(width: 180,height: 180,color: Colors.yellow,
+                      child: Column(
+                        children: [
+                          Expanded(child: 
+                          Image.network('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png',
+                            fit: BoxFit.contain,
+                          ),),
+                          Text(
+                            'Mr.Pikachu',
+                            style: TextStyle(fontSize: 20, color: Colors.black),
+                          ),
+                          const SizedBox(height: 10),
+                        ]
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    _selectCandidateButton('Candidate No.4', is4Checked,
+                                            (val) {setState(() => is4Checked = val!);}
+                                            ),
+                    ],
+                  ),
+
+                ],
+              ),
+
+              // SizedBox(height: 20),
+
+              _comfirmCandidateButton('Confirm'),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget _buildCandidateButton(String label, int candidateId) {
+  Widget _comfirmCandidateButton(String label) {
     return ElevatedButton(
-      onPressed: () => _handleVote(candidateId),
+      onPressed: () {},
       style: ElevatedButton.styleFrom(
         padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 32.0),
         shape: RoundedRectangleBorder(
@@ -117,4 +164,19 @@ class _VotingPageState extends State<VotingPage> {
       child: Text(label, style: const TextStyle(fontSize: 16)),
     );
   }
+
+  Widget _selectCandidateButton(String label, bool option, Function(bool?) onChanged){
+    return SizedBox(
+      width:180,
+      child:CheckboxListTile(
+          title: Text(label, style: const TextStyle(fontSize: 16)),
+          value: option,
+          visualDensity: VisualDensity(horizontal: -4, vertical: -4),
+          contentPadding: EdgeInsets.all(8),
+          onChanged: onChanged,
+        )
+    );
+  }
 }
+
+
